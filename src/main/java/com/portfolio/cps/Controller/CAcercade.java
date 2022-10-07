a70889c77d5db07f27f88ce9ec70af70207af744
candesantos <candelasantos1d@gmail.com>
2022-10-07 13:23:30 -0300

    Deploy
    .

--- src/main/java/com/portfolio/cps/Controller/CAcercade.java
+++ src/main/java/com/portfolio/cps/Controller/CAcercade.java
@@ -26,7 +26,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/acercade")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://frontend-2002.web.app")
 
 public class CAcercade {
     @Autowired
--- src/main/java/com/portfolio/cps/Controller/PersonaController.java
+++ src/main/java/com/portfolio/cps/Controller/PersonaController.java
@@ -23,7 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/personas")
 
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://frontend-2002.web.app")
 
 public class PersonaController {
     @Autowired
